1580710ab356e7f53e5c6ba9fe52749537c319ef
Tam Nguyen Khac <khac.tam.94@gmail.com>
2022-03-26 12:13:23 +0700

    feat: use debouce value

---
+++ src/use-debounced-value/demo.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import { useDebouncedValue } from '@mantine/hooks';
+import { TextInput, Text } from '@mantine/core';
+
+function Demo() {
+  const [value, setValue] = useState('');
+  const [debounced] = useDebouncedValue(value, 200);
+
+  return (
+    <>
+      <TextInput
+        label="Enter value to see debounce"
+        value={value}
+        style={{ flex: 1 }}
+        onChange={(event) => setValue(event.currentTarget.value)}
+      />
+
+      <Text>Value: {value}</Text>
+      <Text>Debounced value: {debounced}</Text>
+    </>
+  );
+}
